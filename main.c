dde76b44374c9857a1f6a9aa8263efe48377e3d9
zxcv <zz29275164@gmail.com>
2018-05-11 10:45:07 +0800

    zxcv

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+		int i,s;
+	for(i=0;i<10;i++)
+	{
+	   s=s+i;
+		
+		
+		
+	}
+	printf("%d",s)	;
+	return 0;
+}
